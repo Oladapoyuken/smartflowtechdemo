e04eb8efea1fe6a1d5fbc9caef0913d72ea5c20c
Oladapo Yusuf <info@nexttechnigeria.com>
2021-12-16 03:56:11 +0100

    Added repository annotation

--- src/main/java/com/smartflowtech/demo/repository/CustomerRepo.java
+++ src/main/java/com/smartflowtech/demo/repository/CustomerRepo.java
@@ -2,10 +2,11 @@ package com.smartflowtech.demo.repository;
 
 import com.smartflowtech.demo.models.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
 import java.util.Optional;
 
+@Repository
 public interface CustomerRepo extends JpaRepository<Customer, Long> {
     Optional<Customer> findByUsername(String username);
 
--- src/main/java/com/smartflowtech/demo/repository/TransactionRepo.java
+++ src/main/java/com/smartflowtech/demo/repository/TransactionRepo.java
@@ -2,7 +2,8 @@ package com.smartflowtech.demo.repository;
 
 import com.smartflowtech.demo.models.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface TransactionRepo extends JpaRepository<Transaction, Long> {
 }
